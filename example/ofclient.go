8b4e1b6efcdad74b809f2454a1b8d7ac7f26fbfa
Raymond Wen <rx.wen218@gmail.com>
2016-10-25 13:32:56 +0800

    add example exe

---
+++ example/ofclient.go
@@ -0,0 +1,28 @@
+// Package main
+package main
+
+import (
+	"fmt"
+
+	openfirerest "github.com/rxwen/go-openfire-rest-client"
+)
+
+func main() {
+	server := "http://127.0.0.1:9090"
+	// calculate authorization echo -n "username:password" | base64
+	authorization := "Basic Calculated code"
+	items, _ := openfirerest.GetRoster(server, authorization, "user")
+	fmt.Println(items)
+
+	item := openfirerest.RosterItem{
+		JID:              "contact",
+		NickName:         "contact",
+		SubscriptionType: 3,
+		Groups: []openfirerest.RosterGroup{
+			{Group: "group"},
+		},
+	}
+	fmt.Println(openfirerest.AddRoster(server, authorization, "user", item))
+	item.SubscriptionType = 2
+	fmt.Println(openfirerest.UpdateRoster(server, authorization, "user", item))
+}
